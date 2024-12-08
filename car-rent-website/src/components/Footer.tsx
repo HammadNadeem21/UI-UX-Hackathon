a1671961a6591cff7a3b19bef98286dc46e0410b
HammadNadeem21 <nhammad424@gmail.com>
2024-12-08 19:46:52 +0500

    updated

--- car-rent-website/src/components/Footer.tsx
+++ car-rent-website/src/components/Footer.tsx
@@ -147,11 +147,11 @@ const PlusJakartaSans600 = Plus_Jakarta_Sans({
 const Footer = () => {
   return (
     <div className="lg:bg-white bg-transparent">
-      <div className="w-full lg:h-[400px] h-[400px] pt-[50px] lg:px-[110px] px-8">
-        <div className="w-full h-auto lg:grid lg:grid-rows-[1fr,100px] lg:grid-cols-2 flex flex-col gap-10 justify-center">
+      <div className="w-full lg:h-[400px] h-[400px] pt-[50px] lg:px-[110px] px-5">
+        <div className="w-full h-auto lg:grid lg:grid-rows-[1fr,100px] grid-rows-[1fr,200px] lg:grid-cols-2 flex flex-col lg:gap-14 md:gap-0 gap-10 justify-center">
           {/* Logo and Description */}
-          <div className="m-auto">
-            <div className="lg:w-[292px] h-auto md:w-[500px] sm:text-center text-left">
+          <div className="lg:m-0 m-auto">
+            <div className="lg:w-[292px] h-auto md:w-[500px] lg:text-left sm:text-center text-left">
               <h1
                 className={`${PlusJakartaSans.className} lg:text-[32px] md:text-[40px] text-[24px] text-[#3563E9]`}
               >
@@ -167,65 +167,65 @@ const Footer = () => {
           </div>
 
           {/* Footer Links Section */}
-          <div className="grid lg:gap-8 gap-4 lg:grid-cols-3 sm:grid-cols-3 grid-cols-2 lg:justify-center">
+          <div className="grid lg:gap-8 gap-4 lg:grid-cols-3 md:grid-cols-3 grid-cols-2 lg:justify-center lg:px-0 md:px-[150px]">
             {/* About */}
-            <div className="w-auto h-auto">
+            <div className="w-auto h-auto pl-2">
               <h1
-                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C]`}
+                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C] cursor-pointer`}
               >
                 About
               </h1>
               <div className="mt-[15px] leading-[30px] pt-4 text-[14px] lg:text-[#13131399] text-[#90A3BF]">
-                <p>How it works</p>
-                <p>Featured</p>
-                <p>Partnership</p>
-                <p>Business Relation</p>
+                <p className="cursor-pointer">How it works</p>
+                <p className="cursor-pointer">Featured</p>
+                <p className="cursor-pointer">Partnership</p>
+                <p className="cursor-pointer">Business Relation</p>
               </div>
             </div>
 
             {/* Community */}
-            <div className="w-auto h-auto">
+            <div className="w-auto h-auto lg:ml-0 ml-[50px]">
               <h1
-                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C]`}
+                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C] cursor-pointer`}
               >
                 Community
               </h1>
               <div className="mt-[15px] leading-[30px] pt-4 text-[14px] lg:text-[#13131399] text-[#90A3BF]">
-                <p>Events</p>
-                <p>Blog</p>
-                <p>Podcast</p>
-                <p>Invite a friend</p>
+                <p className="cursor-pointer">Events</p>
+                <p className="cursor-pointer">Blog</p>
+                <p className="cursor-pointer">Podcast</p>
+                <p className="cursor-pointer">Invite a friend</p>
               </div>
             </div>
 
             {/* Socials */}
-            <div className="w-auto h-auto">
+            <div className="w-auto h-auto lg:ml-0 md:ml-[80px] ml-0">
               <h1
-                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C]`}
+                className={`${PlusJakartaSans600.className} text-[20px] text-[#1A202C] cursor-pointer`}
               >
                 Socials
               </h1>
               <div className="mt-[15px] leading-[30px] pt-4 text-[14px] lg:text-[#13131399] text-[#90A3BF]">
-                <p>Discord</p>
-                <p>Instagram</p>
-                <p>Twitter</p>
-                <p>Facebook</p>
+                <p className="cursor-pointer">Discord</p>
+                <p className="cursor-pointer">Instagram</p>
+                <p className="cursor-pointer">Twitter</p>
+                <p className="cursor-pointer">Facebook</p>
               </div>
             </div>
           </div>
 
           {/* Bottom Section */}
-          <div className="col-span-2 w-full mt-5">
+          <div className="col-span-2 w-full">
             <div className="bg-[#13131329] w-full h-[1px] mt-2 md:block hidden"></div>
 
-            <div className="mt-[20px] flex flex-col md:flex-row justify-between items-center gap-4">
+            <div className="lg:mt-[20px] flex flex-col md:flex-row justify-between items-center gap-4">
               <h1
                 className={`${PlusJakartaSans600.className} lg:text-[16px] text-[13px] text-[#1A202C]`}
               >
                 ©2022 MORENT. All rights reserved
               </h1>
 
-              <div className="flex flex-wrap gap-4">
+               <div className="flex flex-wrap gap-4 sm:mb-0 mb-2">
                 <h2
                   className={`${PlusJakartaSans600.className} lg:text-[16px] text-[13px] text-[#1A202C] tracking-tighter`}
                 >
@@ -237,8 +237,15 @@ const Footer = () => {
                   Terms & Condition
                 </h2>
               </div>
+
+
+
             </div>
           </div>
+
+
+
+
         </div>
       </div>
     </div>
