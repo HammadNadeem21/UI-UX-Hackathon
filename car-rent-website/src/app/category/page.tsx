4f5def4f01b4ec566d2e989f1f4ffd1097ffb9b1
HammadNadeem21 <nhammad424@gmail.com>
2024-12-10 23:17:00 +0500

    updated

--- car-rent-website/src/app/category/page.tsx
+++ car-rent-website/src/app/category/page.tsx
@@ -12,6 +12,7 @@ import { MdFavorite } from "react-icons/md";
 import { BsFillFuelPumpFill } from "react-icons/bs";
 import { TbSteeringWheel } from "react-icons/tb";
 import { FaUserGroup } from "react-icons/fa6";
+import { TbSwitchVertical } from "react-icons/tb";
 
 // Font
 import { Plus_Jakarta_Sans } from "next/font/google";
@@ -36,9 +37,9 @@ const PlusJakartaSans600 = Plus_Jakarta_Sans({
 export default function carRent() {
   return (
     <div>
-      <div className="flex">
+      <div className="grid md:grid-cols-[25%,75%] grid-cols-1">
         {/* sidebar */}
-        <div className="w-[25%]">
+        <div className="w-full">
           <div className="bg-white h-full py-5 px-3 mt-3">
             <div className="w-[176px] h-[302px] ml-5 mt-5">
               <h1
@@ -177,107 +178,120 @@ export default function carRent() {
         </div>
 
         {/* Dashboard */}
-        <div className="w-[75%]">
+        <div className="w-full">
           {/* Pick Up & Drop Up (Start) */}
-          <div className="w-full h-[200px] py-9 px-5 grid grid-cols-[425px,1fr,425px]">
-            <div className="bg-white xl:w-[425px] xl:h-[132px] lg:w-[90%] md:w-[85%] h-[132px] rounded-lg text-[100%] lg:text-[70%]">
-              <div className="px-8 py-3">
-                <input type="radio" />
-                <label htmlFor="" className="ml-2">
-                  Pick-Up
-                </label>
+       
 
-                <div className="w-full h-[85px] grid grid-cols-[1fr,1fr,1fr]">
-                  <div className=" border-[#C3D4E9] border-r-2 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Locations
-                    </h1>
+<div className="lg:h-[190px] h-[380px] px-5 py-3">
 
-                    <div className=" h-[25px] mt-3">
-                      <SelectRent />
-                    </div>
-                  </div>
+<div className="h-[150px] grid xl:grid-cols-[1fr,auto,1fr] lg:grid-cols-[1fr,auto,1fr] xs:grid-cols-[500px] grid-cols-1 justify-center py-3 mt-2">
+  
+  {/* Pick - Up */}
+  <div className={`${PlusJakartaSans600.className} text-[16px] text-[#1A202C] h-[132px] bg-white py-2 xl:px-5 lg:px-[10px] px-5 rounded-[10px]`}>
+  <input type="radio" id="pickUp" />
+      <label htmlFor="pickUp" className="ml-2">
+        Pick-Up
+      </label>
 
-                  <div className=" border-[#C3D4E9] border-r-2 ml-3 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Date
-                    </h1>
 
-                    <div className="w-126px h-[25px] mt-3">
-                      <SelectDemo />
-                    </div>
-                  </div>
-                  <div className="ml-2 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Time
-                    </h1>
+      <div className="w-full h-[85px] grid grid-cols-[1fr,1fr,1fr] xl:gap-3  xs:gap-2 gap-1 mt-[14px]">
 
-                    <div className="w-126px h-[25px] mt-3">
-                      <SelectDemo />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <Image
-              src={Switch}
-              alt="switch"
-              className="mt-10 h-[80px] w-[80px]"
-            />
-
-            <div className="bg-white xl:w-[425px] xl:h-[132px] lg:w-[90%] md:w-[85%] h-[132px] rounded-lg text-[100%] lg:text-[70%]">
-              <div className="px-8 py-3">
-                <input type="radio" />
-                <label htmlFor="" className="ml-2">
-                  Pick-Up
-                </label>
-
-                <div className="w-full h-[85px] grid grid-cols-[1fr,1fr,1fr]">
-                  <div className=" border-[#C3D4E9] border-r-2 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Locations
-                    </h1>
-
-                    <div className="w-106px h-[25px] mt-3">
-                      <SelectRent />
-                    </div>
-                  </div>
-
-                  <div className=" border-[#C3D4E9] border-r-2 ml-3 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Date
-                    </h1>
-
-                    <div className="w-126px h-[25px] mt-3">
-                      <SelectDemo />
-                    </div>
-                  </div>
-                  <div className="ml-2 mt-3">
-                    <h1
-                      className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}
-                    >
-                      Time
-                    </h1>
-
-                    <div className="w-126px h-[25px] mt-3">
-                      <SelectDemo />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+        {/* Locations */}
+        <div className="border-[#C3D4E9] border-r-2 pr-3">
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Locations
+          </h1>
+          <div className="h-[25px] mt-3">
+            <SelectRent name="Select your location" opt1="Karachi" opt2="Lahore"  opt3="Islamabad" opt4="Peshawar" opt5="Multan"/>
           </div>
+        </div>
+
+
+
+        {/* Date */}
+        <div className="border-[#C3D4E9] border-r-2 pr-3">
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Date
+          </h1>
+          <div className="w-full h-[25px] mt-3">
+            <SelectRent name="Select your date" opt1="6" opt2="12" opt3="18" opt4="24" opt5="30"/>
+          </div>
+        </div>
+
+        {/* Time */}
+        <div>
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Time
+          </h1>
+          <div className="w-full h-[25px] mt-3">
+            <SelectRent name="Select your time" opt1="8:00 am" opt2="12:00 pm" opt3="4:00 pm" opt4="8:00 pm" opt5="12:00 am" />
+          </div>
+        </div>
+
+
+        </div>
+  </div>
+
+
+{/* Switch-Button */}
+    <div className="xl:w-[60px] xl:h-[60px] lg:w-[40px] lg:h-[40px] w-[50px] h-[50px] bg-[#3563E9] rounded-md xl:py-[12px] xl:px-[12px] lg:py-2 lg:px-2 py-3 px-3 cursor-pointer lg:mt-10 m-auto">
+    <TbSwitchVertical className="text-white xl:text-[35px] text-[25px]"/>
+    </div>
+
+  
+
+
+  {/* Drop - Off */}
+  <div className=" h-[132px] bg-white py-2 xl:px-5 lg:px-[10px] px-5 rounded-[10px]">
+  <input type="radio" id="pickUp" />
+      <label htmlFor="pickUp" className={`${PlusJakartaSans600.className} text-[16px] text-[#1A202C] ml-2`}>
+        Pick-Up
+      </label>
+
+
+      <div className="w-full h-[85px] grid grid-cols-[1fr,1fr,1fr] xl:gap-3 xs:gap-2 gap-1  mt-[14px]">
+
+        {/* Locations */}
+        <div className="border-[#C3D4E9] border-r-2 pr-3">
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Locations
+          </h1>
+          <div className="h-[25px] mt-3">
+            <SelectRent name="Select your location" opt1="Karachi" opt2="Lahore"  opt3="Islamabad" opt4="Peshawar" opt5="Multan"/>
+          </div>
+        </div>
+
+
+
+        {/* Date */}
+        <div className="border-[#C3D4E9] border-r-2 pr-3">
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Date
+          </h1>
+          <div className="w-full h-[25px] mt-3">
+            <SelectRent name="Select your date" opt1="6" opt2="12" opt3="18" opt4="24" opt5="30" />
+          </div>
+        </div>
+
+        {/* Time */}
+        <div>
+          <h1 className={`${PlusJakartaSans700.className} text-[16px] text-[#1A202C]`}>
+            Time
+          </h1>
+          <div className="w-full h-[25px] mt-3">
+            <SelectRent name="Select your time" opt1="8:00 am" opt2="12:00 pm" opt3="4:00 pm" opt4="8:00 pm" opt5="12:00 am"/>
+          </div>
+        </div>
+
+
+        </div>
+  </div>
+
+
+ 
+</div>
+</div>
+
           {/* Pick Up & Drop Up (End) */}
 
           {/* Cars Section (Start) */}
--- car-rent-website/src/components/Select_rent.tsx
+++ car-rent-website/src/components/Select_rent.tsx
@@ -10,20 +10,20 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 
-export function SelectRent() {
+export function SelectRent(props:{name:string, opt1:string, opt2:string, opt3:string, opt4:string, opt5:string}) {
   return (
     <Select>
-      <SelectTrigger className="xl:w-[70px] md:w-[50px] xs:w-[150px] ms:w-[70px] w-[60px] text-[12p] focus:outline-none bg-transparent h-[30px]">
-        <SelectValue placeholder="Select your city"/>
+      <SelectTrigger className="xl:w-[100px] lg:w-[90px] xs:w-[140px]  ms:w-[80px] w-[60px] text-[12p] focus:outline-none bg-transparent h-[30px]">
+        <SelectValue placeholder={props.name}/>
       </SelectTrigger>
       <SelectContent className="bg-gray-200">
         <SelectGroup>
           {/* <SelectLabel>Fruits</SelectLabel> */}
-          <SelectItem value="Karachi">Karachi</SelectItem>
-          <SelectItem value="Lahore">Lahore</SelectItem>
-          <SelectItem value="Islamabad">Islamabad</SelectItem>
-          <SelectItem value="Peshawar">Peshawar</SelectItem>
-          <SelectItem value="Faislabad">Faislabad</SelectItem>
+          <SelectItem value={props.opt1}>{props.opt1}</SelectItem>
+          <SelectItem value={props.opt2}>{props.opt2}</SelectItem>
+          <SelectItem value={props.opt3}>{props.opt3}</SelectItem>
+          <SelectItem value={props.opt4}>{props.opt4}</SelectItem>
+          <SelectItem value={props.opt5}>{props.opt5}</SelectItem>
         </SelectGroup>
       </SelectContent>
     </Select>
